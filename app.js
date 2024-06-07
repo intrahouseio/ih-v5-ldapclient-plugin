78d4c74f65ddf2cecdbdec582b66a831a160059b
nimagrolis <486016@gmail.com>
2024-06-07 14:47:31 +0300

    v5.0.2

--- app.js
+++ app.js
@@ -4,37 +4,101 @@
  *   Основной модуль плагина
  *
  */
+
 const util = require('util');
 const LdapClient = require('ldapjs-client');
 
+const Parser = require('./lib/parser');
+const utils = require('./lib/utils');
+
 module.exports = async function(plugin) {
-  const { url, login, pass, search_groups, search_users } = plugin.params;
+  const {
+    url,
+    login,
+    pass,
+    search_groups,
+    groups_filter,
+    search_users,
+    users_filter
+  } = plugin.params;
+
+  const parser = new Parser(plugin.params);
 
   const client = new LdapClient({ url });
 
   // bind для проверки
-  await bind();
-  // client.unbind();
+  try {
+    plugin.log('Try bind to ' + url);
+    await client.bind(login, pass);
+    plugin.log('Bind OK');
+    
+  } catch (e) {
+    plugin.exit(1, 'ERROR: Bind to ' + url + ' with dn=' + login + ' failed: ' + util.inspect(e));
+  }
+  
   /*
-  const gresponse = await searchSub(search_groups);
-  plugin.log('Response groups: ' + util.inspect(gresponse));
-
-  const response = await searchSub(search_users);
-  plugin.log('Response people: ' + util.inspect(response));
+  await sleep(2000);
+  
+  try {
+    await client.unbind();
+  } catch (e) {
+    plugin.log('Unbind error!! '+util.inspect(e));
+  }
   */
 
-  async function bind() {
-    try {
-      await client.bind(login, pass);
-      plugin.log('Bind to ' + url + ' OK');
-    } catch (e) {
-      plugin.exit(1, 'ERROR: Bind to ' + url + ': ' + util.inspect(e));
+  plugin.onCommand(async message => {
+    plugin.log('Get command ' + util.inspect(message), 1);
+
+    if (message.param == 'authUser') {
+      const { param, dn, pbx, uuid } = message;
+      try {
+        plugin.log('authUser dn=' + dn + ' pbx=' + pbx);
+        const userPass = Buffer.from(pbx, 'base64').toString();
+        plugin.log('authUser pass=' + userPass);
+        await client.bind(dn, userPass);
+        plugin.log('Bind OK');
+        plugin.sendResponse(message, 1);
+      } catch (e) {
+        const errStr = 'ERROR: Bind with dn=' + dn + ' failed: ' + utils.getErrStrWoTrace(e);
+        plugin.log(errStr);
+        plugin.sendResponse({uuid, dn, param, message:errStr}, 0);
+      }
+      // client.unbind();
+      return;
     }
-  }
+
+    // Для sync* response не отправляем. После получения данных отправляем
+    // {type:'syncGroups', data:[]}  {type:'syncUsers', data:[]}
+
+    if (message.param == 'syncUsers') {
+      try {
+        const data = await getUsersFromLDAP();
+        plugin.send({ type: 'syncUsers', data });
+      } catch (e) {
+        plugin.log('ERROR: getUsersFromLDAP: ' + util.inspect(e));
+      }
+      return;
+    }
+
+    if (message.param == 'syncGroups') {
+      try {
+        const data = await getGroupsFromLDAP();
+        plugin.send({ type: 'syncGroups', data });
+      } catch (e) {
+        plugin.log('ERROR: getGroupsFromLDAP: ' + util.inspect(e));
+      }
+      return;
+    }
+
+    plugin.log('ERROR:Unknown command param: ' + message.param);
+  });
 
   async function searchSub(dn_str, opt = {}) {
     try {
-      return client.search(dn_str, { scope: 'sub', ...opt });
+      await client.bind(login, pass);
+      const result = await client.search(dn_str, { scope: 'sub', ...opt });
+      client.unbind();
+      return result;
     } catch (e) {
       plugin.exit(2, 'ERROR: search ' + dn_str + ': ' + util.inspect(e));
     }
@@ -42,146 +106,48 @@ module.exports = async function(plugin) {
 
   async function getUsersFromLDAP() {
     try {
-      const response = await searchSub(search_users);
+      const attributes = parser.getUserSearchAttributes();
+      plugin.log('search attributes ' + util.inspect(attributes));
+      const response = await searchSub(search_users, { filter: users_filter, attributes });
       if (!response) throw { message: 'No response for LDAP search: ' + search_users };
 
       if (!Array.isArray(response))
-        throw { message: 'Invalid response for search: ' + search_users + util.inspect(response) };
+        throw { message: 'Invalid response for search: ' + util.inspect(response) };
 
-      const data = [];
-      response.forEach(item => {
-        if (item.cn && item.gidNumber) {
-          data.push({ id: item.uid, group: item.gidNumber, name: item.cn });
-        }
-      });
-      return data;
+      plugin.log('response for LDAP search: ' + util.inspect(response));
+      return parser.getMappedUsers(response);
     } catch (e) {
-      return e;
+      throw e;
     }
   }
 
   async function getGroupsFromLDAP() {
     try {
-      const response = await searchSub(search_groups);
+      const attributes = parser.getGroupSearchAttributes();
+      plugin.log('search attributes ' + util.inspect(attributes));
+
+      const response = await searchSub(search_groups, { filter: groups_filter, attributes });
       if (!response) throw { message: 'No response for LDAP search: ' + search_groups };
 
       if (!Array.isArray(response))
-        throw {
-          message: 'Invalid response for search: ' + search_groups + util.inspect(response)
-        };
+        throw { message: 'Bad response for search: ' + util.inspect(response) };
 
-      const data = [];
-      response.forEach(item => {
-        if (item.cn && item.gidNumber) {
-          data.push({ id: item.gidNumber, name: item.cn });
-        }
-      });
-      return data;
+      plugin.log('response for LDAP search: ' + util.inspect(response));
+      return parser.getMappedGroups(response);
     } catch (e) {
-      return e;
+      throw e;
     }
   }
-
-  plugin.onCommand(async message => {
-    plugin.log('Get command ' + util.inspect(message), 1);
-    // Response не отправляем. После получения данных отправляем {type:'syncUsers', data:[]}
-    let result = 'Unknown command param: ' + message.param;
-
-    // await bind();
-    let data;
-    if (message.param == 'syncUsers') {
-      data = await getUsersFromLDAP(message.param);
-    } else if (message.param == 'syncGroups') {
-      data = await getGroupsFromLDAP(message.param);
-    }
-    // client.unbind();
-    result = Array.isArray(data) ? { type: message.param, data } : data;
-
-    if (result.type) {
-      plugin.send(result);
-    } else if (result.message) {
-      plugin.log('ERROR: ' + result.message);
-    } else {
-      plugin.log('ERROR: ' + util.inspect(result));
-    }
-  });
+  
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 };
 
 /*
-groups: [
-  {
-    dn: 'ou=groups,dc=ih-systems,dc=com',
-    ou: 'groups',
-    objectClass: [ 'organizationalUnit', 'top' ]
-  },
-  {
-    dn: 'cn=support,ou=groups,dc=ih-systems,dc=com',
-    cn: 'support',
-    gidNumber: '502',
-    objectClass: [ 'posixGroup', 'top' ]
-  },
-  {
-    dn: 'cn=engineers,ou=groups,dc=ih-systems,dc=com',
-    cn: 'engineers',
-    gidNumber: '501',
-    objectClass: [ 'posixGroup', 'top' ]
-  }
-]
-*/
-/*
-people: [
-  {
-    dn: 'ou=people,dc=ih-systems,dc=com',
-    ou: 'people',
-    objectClass: [ 'organizationalUnit', 'top' ]
-  },
-  {
-    dn: 'cn=ivan ivanov,ou=people,dc=ih-systems,dc=com',
-    givenName: 'ivan',
-    sn: 'ivanov',
-    cn: 'ivan ivanov',
-    uid: 'iivanov',
-    userPassword: '{MD5}4QrcOUm6Wau+VuBX8g+IPg==',
-    uidNumber: '1000',
-    gidNumber: '501',
-    homeDirectory: '/home/users/iivanov',
-    objectClass: [ 'inetOrgPerson', 'posixAccount', 'top' ]
-  },
-  {
-    dn: 'cn=petr petrov,ou=people,dc=ih-systems,dc=com',
-    givenName: 'petr',
-    sn: 'petrov',
-    cn: 'petr petrov',
-    uid: 'ppetrov',
-    userPassword: '{MD5}4QrcOUm6Wau+VuBX8g+IPg==',
-    uidNumber: '1001',
-    gidNumber: '501',
-    homeDirectory: '/home/users/ppetrov',
-    objectClass: [ 'inetOrgPerson', 'posixAccount', 'top' ]
-  },
-  {
-    dn: 'cn=test1 test1,ou=people,dc=ih-systems,dc=com',
-    givenName: 'test1',
-    sn: 'test1',
-    cn: 'test1 test1',
-    uid: 'test1',
-    userPassword: '{MD5}gdyb21LQTcIANtvYMT7QVQ==',
-    uidNumber: '1003',
-    gidNumber: '501',
-    homeDirectory: '/home/users/test1',
-    objectClass: [ 'inetOrgPerson', 'posixAccount', 'top' ]
-  },
-  {
-    dn: 'cn=viktor viktorov,ou=people,dc=ih-systems,dc=com',
-    givenName: 'viktor',
-    sn: 'viktorov',
-    cn: 'viktor viktorov',
-    uid: 'vviktorov',
-    userPassword: '{MD5}4QrcOUm6Wau+VuBX8g+IPg==',
-    uidNumber: '1002',
-    gidNumber: '502',
-    homeDirectory: '/home/users/vviktorov',
-    objectClass: [ 'inetOrgPerson', 'posixAccount', 'top' ]
-  }
-]
-*/
+ login: 'CN=krbtgt,CN=Users,DC=ih-systems,DC=lan',
+  pass: 'fa8c71601196cb7645372c85b60de6d6f602c11c93fa20a7e4209b014d672ca76de99c5526552656c34e4409c751118501a65f5807463a4d649239c09d609925',
+  param: 'authUser',
+  uuid: 'emit_unit_ldapclient_1717424126862',
+  type: 'command'
+  */
