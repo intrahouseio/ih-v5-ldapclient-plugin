9d476d30c5e297e59f2264dd674921f668ecb0e5
nimagrolis <486016@gmail.com>
2024-06-07 17:11:41 +0300

    Remove some old code

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ module.exports = async function(plugin) {
   async function authUser(message) {
     const { param, dn, pbx, uuid } = message;
 
-    plugin.log('authUser dn=' + dn + ' pbx=' + pbx);
+    // plugin.log('authUser dn=' + dn + ' pbx=' + pbx);
     const pw = Buffer.from(pbx, 'base64').toString();
-    plugin.log('authUser pass=' + pw);
+    // plugin.log('authUser pass=' + pw);
 
     const con = new LDAPConnection({ url, dn, pw }, plugin);
     await con.tryBind();
@@ -91,117 +91,4 @@ module.exports = async function(plugin) {
     if (con.error) plugin.log('ERROR: ' + con.error);
     return result;
   }
-
-  /*
-  const client = new LdapClient({ url });
-
-  // bind для проверки
-  try {
-    plugin.log('Try bind to ' + url);
-    await client.bind(login, pass);
-    plugin.log('Bind OK');
-    
-  } catch (e) {
-    plugin.exit(1, 'ERROR: Bind to ' + url + ' with dn=' + login + ' failed: ' + util.inspect(e));
-  }
- 
-
-  plugin.onCommand(async message => {
-    plugin.log('Get command ' + util.inspect(message), 1);
-
-    if (message.param == 'authUser') {
-      const { param, dn, pbx, uuid } = message;
-      try {
-        plugin.log('authUser dn=' + dn + ' pbx=' + pbx);
-        const userPass = Buffer.from(pbx, 'base64').toString();
-        plugin.log('authUser pass=' + userPass);
-        await client.bind(dn, userPass);
-        plugin.log('Bind OK');
-        plugin.sendResponse(message, 1);
-      } catch (e) {
-        const errStr = 'ERROR: Bind with dn=' + dn + ' failed: ' + utils.getErrStrWoTrace(e);
-        plugin.log(errStr);
-        plugin.sendResponse({uuid, dn, param, message:errStr}, 0);
-      }
-      // client.unbind();
-      return;
-    }
-
-    // Для sync* response не отправляем. После получения данных отправляем
-    // {type:'syncGroups', data:[]}  {type:'syncUsers', data:[]}
-
-    if (message.param == 'syncUsers') {
-      try {
-        const data = await getUsersFromLDAP();
-        plugin.send({ type: 'syncUsers', data });
-      } catch (e) {
-        plugin.log('ERROR: getUsersFromLDAP: ' + util.inspect(e));
-      }
-      return;
-    }
-
-    if (message.param == 'syncGroups') {
-      try {
-        const data = await getGroupsFromLDAP();
-        plugin.send({ type: 'syncGroups', data });
-      } catch (e) {
-        plugin.log('ERROR: getGroupsFromLDAP: ' + util.inspect(e));
-      }
-      return;
-    }
-
-    plugin.log('ERROR:Unknown command param: ' + message.param);
-  });
-
-  async function searchSub(dn_str, opt = {}) {
-    try {
-      await client.bind(login, pass);
-      const result = await client.search(dn_str, { scope: 'sub', ...opt });
-      client.unbind();
-      return result;
-    } catch (e) {
-      plugin.exit(2, 'ERROR: search ' + dn_str + ': ' + util.inspect(e));
-    }
-  }
-
-  async function getUsersFromLDAP() {
-    try {
-      const attributes = parser.getUserSearchAttributes();
-      plugin.log('search attributes ' + util.inspect(attributes));
-      const response = await searchSub(search_users, { filter: users_filter, attributes });
-      if (!response) throw { message: 'No response for LDAP search: ' + search_users };
-
-      if (!Array.isArray(response))
-        throw { message: 'Invalid response for search: ' + util.inspect(response) };
-
-      plugin.log('response for LDAP search: ' + util.inspect(response));
-      return parser.getMappedUsers(response);
-    } catch (e) {
-      throw e;
-    }
-  }
-
-  async function getGroupsFromLDAP() {
-    try {
-      const attributes = parser.getGroupSearchAttributes();
-      plugin.log('search attributes ' + util.inspect(attributes));
-
-      const response = await searchSub(search_groups, { filter: groups_filter, attributes });
-      if (!response) throw { message: 'No response for LDAP search: ' + search_groups };
-
-      if (!Array.isArray(response))
-        throw { message: 'Bad response for search: ' + util.inspect(response) };
-
-      plugin.log('response for LDAP search: ' + util.inspect(response));
-      return parser.getMappedGroups(response);
-    } catch (e) {
-      throw e;
-    }
-  }
-  
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-};
-*/
 };
